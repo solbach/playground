984ad87b8b36abd5f41da31b4f5f5ba10665078f
solbach <mksolb@gmail.com>
2015-08-27 12:44:50 -0400

    playground is getting extended by some pcl tools

--- src/playground.cpp
+++ src/playground.cpp
@@ -1,18 +1,31 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include "sensor_msgs/PointCloud2.h"
 
-void chatterCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+/* Specifically for PCL usage */
+#include "sensor_msgs/PointCloud2.h"
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+ros::Publisher pub;
+
+void cloudPub (const sensor_msgs::PointCloud2ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%i]", msg->row_step);
+    sensor_msgs::PointCloud2 output;
+    output = *msg;
+
+    ROS_INFO("I heard: [%i]", msg->width);
+    pub.publish(output);
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "listener");
+  ros::init(argc, argv, "playground");
   ros::NodeHandle n;
 
-  ros::Subscriber sub = n.subscribe("/rtabmap/cloud_map", 1000, chatterCallback);
+  ros::Subscriber sub = n.subscribe("/rtabmap/cloud_map", 1, cloudPub);
+  pub = n.advertise<sensor_msgs::PointCloud2> ("playground/output", 1);
+
   ros::spin();
 
   return 0;
