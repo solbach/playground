3039fe243bf52baa5accc1174ccdc732d881708f
solbach <mksolb@gmail.com>
2015-08-28 14:58:49 -0400

    Floor filtering  using morphology

--- src/playground.cpp
+++ src/playground.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <vector>
 
+/* DEBUG */
+#include <ctime>
+
 /* ROS */
 #include "ros/ros.h"
 #include "std_msgs/String.h"
@@ -32,15 +35,48 @@
 
 ros::Publisher pub;
 pcl::visualization::CloudViewer viewer ("Segmentation Viewer");
+//pcl::visualization::CloudViewer grd_viewer ("Ground Viewer");
 
 void cloudSegmentation (const sensor_msgs::PointCloud2ConstPtr& msg)
 {
+    const clock_t t0=clock();
+
     ROS_INFO("convert ros_msg to pcl");
+    /* base cloud */
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new
                     pcl::PointCloud<pcl::PointXYZ>());
+    /* convert ros message to base cloud */
     pcl::fromROSMsg(*msg, *cloud);
 
-    ROS_INFO("Segmentation");
+//    viewer.showCloud( cloud );
+
+    /* filtered cloud */
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new
+                    pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointIndicesPtr ground (new pcl::PointIndices);
+
+    ROS_INFO("Ground Filtering");
+    /* create filter object and extract ground */
+
+    pcl::ProgressiveMorphologicalFilter<pcl::PointXYZ> pmf;
+    pmf.setInputCloud(cloud);
+    pmf.setMaxWindowSize(20);
+    pmf.setSlope(1.0f);
+    pmf.setInitialDistance(0.5f);
+    pmf.setMaxDistance(3.0f);
+    pmf.extract(ground->indices);
+    /* extract ground from cloud and save to cloud_filtered */
+    pcl::ExtractIndices<pcl::PointXYZ> extract;
+    extract.setInputCloud(cloud);
+    extract.setIndices(ground);
+    extract.filter(*cloud);
+    extract.setNegative(true);
+    extract.filter(*cloud_filtered);
+
+//    viewer.showCloud( cloud_filtered );
+
+//    ROS_INFO("Segmentation");
+    /* create segmentation object */
     pcl::search::Search<pcl::PointXYZ>::Ptr tree =
             boost::shared_ptr<pcl::search::Search<pcl::PointXYZ> >
             (new pcl::search::KdTree<pcl::PointXYZ>);
@@ -48,13 +84,13 @@ void cloudSegmentation (const sensor_msgs::PointCloud2ConstPtr& msg)
             (new pcl::PointCloud <pcl::Normal>);
     pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimator;
     normal_estimator.setSearchMethod (tree);
-    normal_estimator.setInputCloud (cloud);
+    normal_estimator.setInputCloud (cloud_filtered);
     normal_estimator.setKSearch (50);
     normal_estimator.compute (*normals);
 
     pcl::IndicesPtr indices (new std::vector <int>);
     pcl::PassThrough<pcl::PointXYZ> pass;
-    pass.setInputCloud (cloud);
+    pass.setInputCloud (cloud_filtered);
     pass.setFilterFieldName ("z");
     pass.setFilterLimits (0.0, 1.0);
     pass.filter (*indices);
@@ -64,7 +100,7 @@ void cloudSegmentation (const sensor_msgs::PointCloud2ConstPtr& msg)
     reg.setMaxClusterSize (300);
     reg.setSearchMethod (tree);
     reg.setNumberOfNeighbours (30);
-    reg.setInputCloud (cloud);
+    reg.setInputCloud (cloud_filtered);
     //reg.setIndices (indices);
     reg.setInputNormals (normals);
     reg.setSmoothnessThreshold (10.0 / 180.0 * M_PI);
@@ -72,6 +108,9 @@ void cloudSegmentation (const sensor_msgs::PointCloud2ConstPtr& msg)
 
     std::vector <pcl::PointIndices> clusters;
     reg.extract (clusters);
+
+    std::cout << float( clock () - t0 ) /  CLOCKS_PER_SEC
+              << ", " << msg->width << std::endl;
 
     std::cout << "Number of clusters is equal to " << clusters.size()
               << std::endl;
